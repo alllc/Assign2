38cd5a32e22028dd6c67c7866250856146337d55
alllc <llx789456@hotmail.com>
2016-10-06 18:26:14 -0700

    trigger

---
+++ Assets/De.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class De : MonoBehaviour {
+	public GameObject other;
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject.tag == "Player"){
+			Debug.Log ("DDD");
+			Destroy (other);
+
+		}
+	}
+
+}
